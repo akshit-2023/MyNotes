b981bb7ef3a505a228d4920f499e8c787735579d
Akshit <akshitv2004@gmail.com>
2024-09-30 04:58:25 +0530

    step-7

--- lib/views/register_view.dart
+++ lib/views/register_view.dart
@@ -1,8 +1,7 @@
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
-import 'dart:developer' as devtools show log;
-
 import 'package:mynotes/constants/routes.dart';
+import 'package:mynotes/utilities/show_error_dialog.dart';
 
 class RegisterView extends StatefulWidget {
   const RegisterView({super.key});
@@ -55,20 +54,27 @@ class _RegisterViewState extends State<RegisterView> {
                   final email = _email.text;
                   final password = _password.text;
                   try{
-                    final userCredential = await FirebaseAuth.instance
+                    await FirebaseAuth.instance
                       .createUserWithEmailAndPassword(
                           email: email, password: password);
-                  devtools.log(userCredential.toString());
+                    final user=FirebaseAuth.instance.currentUser;
+                    await user?.sendEmailVerification();//await because this function returns a Future
+                    Navigator.of(context).pushNamed(verifyEmailRoute);//pushNamed is used because we dont want to remove the page before the verifyemailroute as if we registered using wrong email we can go back.
                 }on FirebaseAuthException catch(e){
                   if(e.code=='weak-password'){
-                    devtools.log("Weak password");
+                    await showErrorDialog(context, 'Weak password');
                   }
                   else if(e.code=='email-already-in-use'){
-                    devtools.log("E-mail is already in use");
+                    await showErrorDialog(context, 'E-mail is already in use');
                   }
                   else if(e.code=='invalid-email'){
-                    devtools.log("Invalid E-mail entered");
+                    await showErrorDialog(context, 'Invalid E-mail');
                   }
+                  else{
+                    await showErrorDialog(context, 'Error: ${e.code}');
+                  }
+                } catch(e){
+                  await showErrorDialog(context, e.toString());
                 }
                   }
                   ,
--- lib/views/verify_email_view.dart
+++ lib/views/verify_email_view.dart
@@ -1,5 +1,6 @@
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
+import 'package:mynotes/constants/routes.dart';
 
 class VerifyEmailView extends StatefulWidget {
   const VerifyEmailView({super.key});
@@ -14,12 +15,16 @@ class _VerifyEmailViewState extends State<VerifyEmailView> {
     return Scaffold(
       appBar: AppBar(title: const Text("Verify E-mail"),),
       body: Column(children: [//changed from return scaffold to return column because of Navigator error
-        const Text("Please verify your E-mail address:"),
+        const Text("E-mail verification sent, if not received click the button below to resend it."),
         TextButton(onPressed: () async {//try to make child the last argument in TextButton,await used so async also used
           final user=FirebaseAuth.instance.currentUser;
           await user?.sendEmailVerification();//returns Future so await is used
         }, 
-        child: const Text("Send E-mail verification"))
+        child: const Text("Send E-mail verification")),
+        TextButton(onPressed:() async {
+          await FirebaseAuth.instance.signOut();
+          Navigator.of(context).pushNamedAndRemoveUntil(registerRoute,(route)=> false);
+        }, child: const Text("Restart")),
       ],),
     );
   }
